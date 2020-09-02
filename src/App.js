3a29a14bf84bfde353dc1d42028e646cdf777ab9
ChayaGreisman <chayamgreisman@gmail.com>
2020-09-02 11:20:44 -0400

    basic landing page start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">Active</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+
+      <img src="./mepic.png" alt="chaya-greisman"/>
+      
+      <h1 style={{fontFamily: 'Raleway'}}>Hey! I'm Chaya.</h1>
     </div>
   );
 }
