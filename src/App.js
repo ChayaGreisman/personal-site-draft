dce38d2500dc9c5b7a8085455d023d322130ab05
ChayaGreisman <chayamgreisman@gmail.com>
2020-09-03 19:34:57 -0400

    profile pic

--- src/App.js
+++ src/App.js
@@ -4,26 +4,9 @@ import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
-    <div className="App">
-
-      <ul className="nav justify-content-center">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Active</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-
-      <img src="./mepic.png" alt="chaya-greisman"/>
-      
-      <h1 style={{fontFamily: 'Raleway'}}>Hey! I'm Chaya.</h1>
+    <div className="App" >
+            <img src="./mepic2.png" alt="chaya-greisman"/>
+            <h1 style={{fontFamily: 'Raleway'}}>Hey! I'm Chaya.</h1>
     </div>
   );
 }
